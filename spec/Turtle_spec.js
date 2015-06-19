2a496d48987892e49c3636d67eb69a5027575515
theory friction practice <jackson.brebnor@gmail.com>
2015-06-19 10:21:37 -0400

    all tessts passing, next step figuring out SVG/D3js

--- spec/Turtle_spec.js
+++ spec/Turtle_spec.js
@@ -12,6 +12,11 @@ describe('Turtle', function() {
             expect(myTurtle.sy).toBe(60);
 
         });
+
+        it('initializes with a startPoint vector', function() {
+            expect(myTurtle.startPoint).toEqual([50, 60]);
+
+        });
         it('initializes with a start direction ', function() {
             expect(myTurtle.heading).toBe(0);
 
@@ -21,24 +26,86 @@ describe('Turtle', function() {
 
         });
         it('initializes with a depth of 0', function() {
-        		expect(myTurtle.depth).toBe(0);
-        	});
+            expect(myTurtle.depth).toBe(0);
+        });
         it('initializes with a generator', function() {
             expect(myTurtle.generator).toBe(myGen);
 
         });
+        it('initializes with an empty array of points', function() {
+            expect(myTurtle.points).toEqual([
+                [50, 60]
+            ]);
+
+        });
 
         describe('generator dependent variables', function() {
-        	it('initializes with a startString', function() {
-        		expect(myTurtle.string).toBe(myGen.startString);
-        	});
-        	it('initializes with an mFactor', function() {
-        		expect(myTurtle.mFactor).toBe(1/3);
-        	});
-        	it('initializes with a baseAngle', function() {
-        		expect(myTurtle.baseAngle).toBe(myGen.baseAngle);
-        	});
-        	
+            it('initializes with a startString', function() {
+                expect(myTurtle.string).toBe(myGen.startString);
+            });
+            it('initializes with an mFactor', function() {
+                expect(myTurtle.mFactor).toBe(1 / 3);
+            });
+            it('initializes with a baseAngle', function() {
+                expect(myTurtle.baseAngle).toBe(myGen.baseAngle);
+            });
+
         });
     });
+
+    describe('#moveForward', function() {
+        it('chnages the position of the turtle based on magnitude and heading', function() {
+            myTurtle.moveForward(myTurtle.startPoint);
+            expect(myTurtle.position).toEqual([150, 60]);
+        });
+
+    });
+
+    describe('#turnLeft', function() {
+        it('increments the heading by the baseAngle', function() {
+            myTurtle.turnLeft();
+            expect(myTurtle.heading).toEqual(Math.PI / 3);
+        });
+
+    });
+
+    describe('#turnRight', function() {
+        it('decrements the heading by the baseAngle', function() {
+            myTurtle.turnRight();
+            myTurtle.turnRight();
+            expect(myTurtle.heading).toEqual(-1 * (Math.PI / 3));
+        });
+
+    });
+    describe('#getPoints', function() {
+        it('converts the string attribute into a series of points via movements', function() {
+            myTurtle.getPoints();
+            expect(myTurtle.points.length).toEqual(5);
+
+        });
+    });
+    describe('#incrementDepth', function() {
+        it('increments the depth attribute by an integer amount', function() {
+            myTurtle.incrementDepth(1);
+            expect(myTurtle.depth).toEqual(1);
+
+        });
+    });
+
+    describe('#getStringAtDepth', function() {
+        it('retrieves new string from generator based on depth argument', function() {
+            myTurtle.getStringAtDepth(2);
+            expect(myTurtle.string).toEqual("a+a--a+a+a+a--a+a--a+a--a+a+a+a--a+a--a+a--a+a+a+a--a+a--a+a--a+a+a+a--a+a--a+a--a+a+a+a--a+a--a+a--a+a+a+a--a+a--");
+        });
+        it('increments the depth attribute by depth argument', function() {
+            expect(myTurtle.depth).toEqual(3);
+
+        });
+    });
+
+    // describe('#visualize', function() {
+    // 	it('produces an SVG visualization of the screen', function() {
+    // 		//not sure how to check d3js svg objects
+    // 	});
+    // });
 });
--- src/Turtle.js
+++ src/Turtle.js
@@ -1,13 +1,64 @@
-incluce
-
-function Turtle (sx, sy, sDirection, sMag, sGen) {
-	this.sx = sx;
-	this.sy = sy;
-	this.heading = sDirection;
-	this.magnitude = sMag;
-	this.generator = sGen;
-	this.string = this.generator.startString;
-	this.mFactor = this.generator.mFactor;
-	this.baseAngle = this.generator.baseAngle;
-	this.depth = 0;
+function Turtle(sx, sy, sDirection, sMag, sGen) {
+    this.sx = sx;
+    this.sy = sy;
+    this.heading = sDirection;
+    this.magnitude = sMag;
+    this.generator = sGen;
+    this.string = this.generator.startString;
+    this.mFactor = this.generator.mFactor;
+    this.baseAngle = this.generator.baseAngle;
+    this.depth = 0;
+    this.points = [];
+    this.startPoint = [this.sx, this.sy];
+    this.points.push(this.startPoint);
+    this.position = this.startPoint;
 }
+
+Turtle.prototype.turnLeft = function() {
+    this.heading += this.baseAngle;
+};
+
+Turtle.prototype.turnRight = function() {
+    this.heading -= this.baseAngle;
+};
+
+Turtle.prototype.moveForward = function(point) {
+    var coefficient = Math.pow(this.mFactor, this.depth);
+    var newMag = coefficient * this.magnitude;
+    var xVector = newMag * Math.cos(this.heading);
+    var yVector = newMag * Math.sin(this.heading);
+    // var xVal = point[0] + xVector;
+    // var yVal = point[1] + yVector;
+    var xVal = this.position[0] + xVector;
+    var yVal = this.position[1] + yVector;
+
+    var newPoint = [xVal, yVal];
+    this.points.push(newPoint);
+    this.position = newPoint;
+};
+Turtle.prototype.getPoints = function() {
+    var splitString = this.string.split("");
+    splitString.forEach(function(el, id, arr) {
+        if (el == "a") {
+            this.moveForward(this.position);
+        } else if (el == "-") {
+            this.turnLeft();
+        } else if (el == "+") {
+            this.turnRight();
+        }
+    }, this);
+
+    console.log(this.points);
+};
+
+Turtle.prototype.incrementDepth = function(delta) {
+    this.depth += delta;
+};
+
+ Turtle.prototype.getStringAtDepth = function(d) {
+            this.string = this.generator.genString(this.string, d);
+            // var tempString = this.generator.genString(this.string, d);
+            // this.string = tempString;
+            this.incrementDepth(d);
+
+        };
